0e15aa94071f32284f0a0295bff19270f816abdc
Neo <s2482818@student.ctihe.edu.hk>
2026-09-23 00:54:20 -0400

    update print A

--- src/lab_68662FBB.c
+++ src/lab_68662FBB.c
@@ -3,7 +3,13 @@
 void printscreen()
 {
     // Please write your code here
-    printf("Hello World\n");
+    printf("  #### \n");
+    printf(" #    #\n");
+    printf(" #    #\n");
+    printf(" ######\n");
+    printf(" #    #\n");
+    printf(" #    #\n");
+    printf(" #    #\n");
 }
 
 // DO NOT change the code below
